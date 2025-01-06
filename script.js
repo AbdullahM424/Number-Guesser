84158cc8fdb5256cd5d51ed5a525a10a3c9d4eef
Abdullah Almeshhen <abmeshhen21@gmail.com>
2025-01-06 19:43:16 +0300

    first version

---
+++ script.js
@@ -0,0 +1,38 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Generate Target function
+function generateTarget(){
+    return Math.floor(Math.random()*10);
+};
+
+// Comparison function 
+function compareGuesses(humanGuess, computerGuess, targetGuess){
+    if(humanGuess === targetGuess){
+        return true;
+    } 
+    else if (computerGuess === targetGuess){
+        return false;
+    }
+    else {
+        if (Math.abs(targetGuess - humanGuess) < Math.abs(targetGuess - computerGuess)){
+            return true;
+        }
+        else if (Math.abs(targetGuess - humanGuess)  Math.abs(targetGuess - computerGuess)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+// Update score function
+function updateScore(winner){
+    winner === 'human' ? humanScore++ : computerScore++;
+};
+
+function advanceRound(){
+    return currentRoundNumber++;
+};
